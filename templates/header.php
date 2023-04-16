bb0c5bd995e16cc9a6e92204c8b730c27072e31a
Boaz <webevolution13@gmail.com>
2023-04-16 13:17:58 +0200

    front end
    external code

---
+++ templates/header.php
@@ -0,0 +1,113 @@
+<!--Preloader-->
+
+<div class="loader" id="loader-fade">
+    <div class="loader-wrapper">
+        <img src="restaurant/img/loader.png" alt="image">
+    </div>
+</div>
+<!--End Preloader-->
+
+
+<!--Header Start-->
+<header class="cursor-light">
+
+    <!--Navigation-->
+    <nav class="navbar navbar-top-default nav-radius navbar-expand-lg">
+        <div class="container">
+            <a href="javascript:void(0)" title="Logo" class="logo scroll">
+                <!--Logo Default-->
+                <img src="restaurant/img/logo.png" alt="logo" class="logo-dark logo-default">
+                <img src="restaurant/img/logo2.png" alt="logo" class="logo-light logo-scrolled">
+            </a>
+
+            <!--Nav Links-->
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link scroll" href="#home">Home</a>
+                    <a class="nav-link scroll" href="#about">About</a>
+                    <a class="nav-link scroll" href="#reservation">Reservation</a>
+                    <a class="nav-link scroll" href="#ourmenu">Our Menu</a>
+                    <a class="nav-link scroll" href="#ourblog">Our Blog</a>
+                    <a class="nav-link scroll" href="#contactus">Contact us</a>
+                </div>
+                <div> <span class="open_search"><i class="fas fa-search"></i> </span></div>
+
+                <div class="search_block">
+                    <div class="search_box animated wow fadeInUp">
+                        <div class="inner">
+                            <input type="text" name="search" id="search" class="search_input" autocomplete="off"
+                                placeholder="Enter Your Keywords.." />
+
+                            <button class="search_icon glyphicon glyphicon-search"><i class="fas fa-search"></i>
+                            </button>
+
+                        </div>
+                    </div>
+                    <div class="search-overlay"></div>
+
+                </div>
+
+                <!-- search input-->
+            </div>
+
+        </div>
+
+        <!--Side Menu Button-->
+        <a href="javascript:void(0)" class="parallax-btn sidemenu_btn" id="sidemenu_toggle">
+            <div class="animated-wrap sidemenu_btn_inner">
+                <div class="animated-element">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </a>
+    </nav>
+
+    <!--Side Nav-->
+    <div class="side-menu">
+        <div class="inner-wrapper">
+            <span class="btn-close link" id="btn_sideNavClose"></span>
+            <nav class="side-nav w-100">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#reservation">Reservation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#gallery">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#ourblog">Our Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  scroll" href="#contactus">Contact us</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="side-footer text-white w-100">
+                <ul class="social-icons-simple">
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i
+                                class="fab fa-facebook-f"></i> </a> </li>
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i
+                                class="fab fa-instagram"></i> </a> </li>
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i
+                                class="fab fa-twitter"></i> </a> </li>
+                </ul>
+                <p class="text-white">&copy; 2023 MegaOne. Made With Love by Themesindustry</p>
+            </div>
+        </div>
+    </div>
+    <a id="close_side_menu" href="javascript:void(0);"></a>
+    <!-- End side menu -->
+
+
+
+</header>
+<!--Header end-->
--- templates/layout.php
+++ templates/layout.php
@@ -4,10 +4,39 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?php echo 'Chaj gallery & Cafe ח״י - גלריה ובית קפה כשר - ' . $title;  ?></title>
+    <title><?php echo 'iGsm box - ' . $title; ?></title>
+    <meta property="og:title" content="iGSM BOX" />
+    <meta property="og:image" content="KaraKrisz/img/logoigb.jpg" />
+    <meta property="og:site_name" content="iGSM BOX" />
+    <meta property="og:description" content="Ha értesülni szeretnél havi akcióinkról, vagy termékeink újítását kísérnéd figyelemmel,
+        akkor kövess minket. Folyamatosan bővül kínáltunk, legkorszerűbb termékeket láthatod a legkedvezőbb áron!" />
+
+    <link rel="schema.dcterms" href="http://purl.org/dc/terms/">
+    <meta name="DC.coverage" content="Hungary" />
+    <meta name="DC.description" content="Ha értesülni szeretnél havi akcióinkról, vagy termékeink újítását kísérnéd figyelemmel,
+        akkor kövess minket. Folyamatosan bővül kínáltunk, legkorszerűbb termékeket láthatod a legkedvezőbb áron!" />
+    <meta name="DC.format" content="text/html" />
+    <meta name="DC.publisher" content="iGSM BOX" />
+    <meta name="DC.title" content="Keszi mobilos" />
+    <meta name="DC.type" content="website" />
+    <!-- Favicon -->
+    <link rel="icon" href="restaurant/img/favicon.ico">
+    <!-- Bundle -->
+    <link rel="stylesheet" href="vendor/css/bundle.min.css">
+    <!-- Plugin Css -->
+    <link rel="stylesheet" href="vendor/css/revolution-settings.min.css">
+    <link rel="stylesheet" href="vendor/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="vendor/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendor/css/swiper.min.css">
+    <link rel="stylesheet" href="vendor/css/cubeportfolio.min.css">
+    <link rel="stylesheet" href="vendor/css/LineIcons.min.css">
+    <!-- Style Sheet -->
+    <link rel="stylesheet" href="vendor/css/select2.min.css">
+    <link rel="stylesheet" href="vendor/css/jquery-ui.bundle.css">
+    <link rel="stylesheet" href="restaurant/css/style.css">
 </head>
 
-<body>
+<body data-spy="scroll" data-target=".navbar" data-offset="90" class="position-relative">
     <?php
     require_once "header.php";
     require_once "$view.php";
